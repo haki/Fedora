c0d9d9e33f8161580f81d1a42c1321e986134cb0
Hakan Meral <hakan@meral.md>
2024-03-23 11:36:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -127,9 +127,4 @@ sudo dnf upgrade --refresh
 sudo dnf check
 sudo dnf autoremove
 
-sudo fwupdmgr get-devices
-sudo fwupdmgr refresh --force
-sudo fwupdmgr get-updates
-sudo fwupdmgr update
-
 sudo reboot now
